37f397143ceee294c497a3f80b96cc3ad9ee83ca
EMPER0RQ <140305075+EMPER0RQ@users.noreply.github.com>
2023-11-23 22:53:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ if len(BOT_TOKEN) == 0:
 PORT = int(environ.get('PORT', '8080'))
 
 # Bot pics
-PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/0514dbfbc128e228d6214.jpg https://telegra.ph/file/2659049d7b6782faf8468.jpg https://telegra.ph/file/356fb2b134d43d918dbd0.jpg https://telegra.ph/file/0bd2e2452a423e6e6393d.jpg')).split()
 
 # Bot Admins
 ADMINS = environ.get('ADMINS', '5311466386')
